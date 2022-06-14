38ff34767ca9bd00ccb97d7a727bb2e725b80044
Tanya Kuchma <kuchma.tanya@ukr.net>
2022-06-14 18:24:20 +0300

    practice

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { v4 } = require("uuid");
 
 const products = require("../../data/products");
 
@@ -36,7 +37,15 @@ router.get("/:id", (req, res) => {
 
 //POST /api/products
 router.post("/", (req, res) => {
-    console.log(req.body)
+    const newProduct = { ...req.body, id: v4() };
+    products.push(newProduct);
+    res.status(201).json({
+        status: "success",
+        code: 201,
+        data: {
+            result: newProduct
+        }
+    })
 })
 
 module.exports = router;
